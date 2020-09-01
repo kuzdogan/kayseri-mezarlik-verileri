5c7cc5f38567a8a13c05e9741919eee6cc4713c0
kuzdogan <kaanuzdogan@hotmail.com>
2020-09-02 01:06:54 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Kayseri Mezarlık Verileri
+
+Özellikle Ağustos ayı itibariyle Türkiye genelinde ve Kayseri ilinde hissedilir şekilde artış gösteren COVID-19 vaka ve vefat sayıları resmi rakamlarının resmi rakamlara yansımadığı şüphesiyle [Kayseri Mezarlık Bilgi Sistemi](https://cbs.kayseri.bel.tr/kayseri-mezarlik-bilgi-sistemi)'nden veriler 'scrape' edilerek çekilmiştir.
+
+Program, belirlenen gün kadar önceden (`DAYS_AGO` değişkeni) güncel tarihe kadar her güne ait rakamlar 2020, 2019 ve 2018 yıllarını yansıtacak şekilde `kayseri-data.csv` dosyasına çıkartır. Her bir tarihe ait web sayfası görüntülerini `pdfs/` ve `screenshots/` klasörlerine kaydeder. Repo'da yalnızca pdfler bulunmaktadır.
+
+Ayrıca `kayseri-data-processed.xlsx` dosyasında veriler birleştirilip 7 günlük ağırlıklı ortalama verileri (moving average) `kayseri-graph.png` grafiğine çıkarılmıştır. Grafik aşağıda görülmektedir. Açıkça görülmektedir ki Ağustos ayı başından beri günlük vefat eden kişi sayısı Kayseri ilinde 2018 ve 2019 yılları ortalmalarından 10-15 kişi fazla gerçekleşmektedir. Benzer rakamlar salgının yoğun yaşandığı, ve resmi rakamlarda da kamuoyuna açıklandığı şekilde, Nisan ayında da ortalamanın üzerindedir. 2020 yılında Kayseri merkezinde 01.09 itibari ile 2019 ve 2018 yılları ortalamalarından 212 kişi fazla vefat etmiştir. Kayseri nufusu ağırlığı hesaba katıldığında Türkiye geneli 12500 rakamı ortaya çıkmaktadır.
+
+Kamuoyunda da konuşulduğu üzere, çekilen rakamlardan da görebildiğimiz gibi, resmi rakamların gerçek verileri yansıtmadığı şüphesi mevcuttur. Veriler yalnızca Kayseri ili merkezi için geçerli olup diğer illerin de hesaba katılması vefat sayılarının resmi rakamlardan uzak olacağı aşikardır. 
+
+Bu rakamlar (mezarlık bilgi sistemi erişime kapatılmadığı sürece) periyodik olarak güncellencektir. Verileri teyit etmek isteyen kişiler her bir tarihte vefat eden kişileri ve kişi sayılarını veriler ile karşılaştırabilir, bu programı indirip verileri kendisi çekebilirler. 
---
+++ index.js
@@ -0,0 +1,143 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const moment = require('moment');
+const csv = require('fast-csv');
+
+const csvStream = csv.format({ headers: true });
+const fsWriteStream = fs.createWriteStream('kayseri-data.csv');
+
+const URL = 'https://cbs.kayseri.bel.tr/kayseri-mezarlik-bilgi-sistemi';
+const DAYS_AGO = 5; // Number of days to go back.
+const CITY_NAME = 'Kayseri';
+
+// Main func
+(async () => {
+  console.log('Launching Browser');
+  // const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
+
+  // Load page
+  console.log('Opened new page');
+  const page = await browser.newPage();
+  console.log('Going to the URL');
+  await page.goto(URL);
+  console.log('Loaded page');
+
+  console.log('Clicking menu button')
+  await page.click('#aspnetForm > div.page > div.header > div.hideSkiplink > div > ul:nth-child(4) > li > a');
+  console.log('Clicked menu button');
+
+  // let date = moment('2020-08-29', 'YYYY-MM-DD');
+  let date = moment().subtract(DAYS_AGO, 'days'); // First date
+
+  fsWriteStream.on('error', function (err) {
+    console.error(err);
+  });
+  csvStream.pipe(fsWriteStream).on('end', () => process.exit());
+
+  // Iterate back over days
+  for (i = 0; i < DAYS_AGO; i++) { // Most recent date first
+    date.add(1, 'days'); // Go one day forward.
+    let date2020Str = date.format('DD.MM.YYYY');
+    let date2020Filename = date.format('YYYY-MM-DD');
+    await changeDateAndSearch(page, date2020Str);
+    let count2020 = await countElements(page);
+    await scrollToTopAndScreenshot(page, date2020Filename, CITY_NAME);
+    await savePagePDF(page, date2020Filename, CITY_NAME);
+
+    // 2019 data
+    let date2019 = date.clone().subtract(1, 'years');
+    let date2019Str = date2019.format('DD.MM.YYYY');
+    let date2019Filename = date2019.format('YYYY-MM-DD');
+    await changeDateAndSearch(page, date2019Str);
+    let count2019 = await countElements(page);
+    await scrollToTopAndScreenshot(page, date2019Filename, CITY_NAME);
+    await savePagePDF(page, date2019Filename, CITY_NAME);
+
+    // 2018 data
+    let date2018 = date.clone().subtract(2, 'years');
+    let date2018Str = date2018.format('DD.MM.YYYY');
+    let date2018Filename = date2018.format('YYYY-MM-DD');
+    await changeDateAndSearch(page, date2018Str);
+    let count2018 = await countElements(page);
+    await scrollToTopAndScreenshot(page, date2018Filename, CITY_NAME);
+    await savePagePDF(page, date2018Filename, CITY_NAME);
+
+    console.log('==========================');
+    console.log('\tTarih: ', date2020Str);
+    console.log('\tVefat sayisi: ', count2020);
+    console.log('\tTarih: ', date2019Str);
+    console.log('\tVefat sayisi: ', count2019);
+    console.log('\tTarih: ', date2018Str);
+    console.log('\tVefat sayisi: ', count2018);
+    csvStream.write({ Tarih: date.format('DD.MM'), VefatSayisi2020: count2020, VefatSayisi2019: count2019, VefatSayisi2018: count2018 });
+  }
+  csvStream.end();
+  await browser.close();
+})();
+
+
+/**
+ * Function to change the input date field and simulate click to search.
+ * 
+ * @param {Object} page - Puppeteer page object 
+ * @param {String} dateStr - Date as DD.MM.YYYY string 
+ * @param {Boolean} debug - Boolean to wait 5s between each instruction
+ * @returns {Promise} That resolves once search is clicked, response received, and DOM updated.
+ */
+async function changeDateAndSearch(page, dateStr, debug = false) {
+  const WAIT = 5000;
+  console.log('Changing input');
+  debug && await new Promise(resolve => setTimeout(resolve, WAIT));
+  await page.focus('#ctl00_MainContent_tlIlanTarihi_dateInput_text');
+  await page.keyboard.down('Control');
+  await page.keyboard.press('A');
+  await page.keyboard.up('Control');
+  await page.keyboard.type(dateStr);
+  console.log('Changed input');
+
+  // Focus and press space instead of clicking. Simple click does not work. 
+  console.log('Focusing Search')
+  debug && await new Promise(resolve => setTimeout(resolve, WAIT));
+  await page.focus('#ctl00_MainContent_btnAraVefat');
+  console.log('Focused Search')
+  console.log('Clicking Search')
+  debug && await new Promise(resolve => setTimeout(resolve, WAIT));
+
+  // Wait for click, response, and DOM change to new date.
+  return Promise.all([
+    page.keyboard.press('Space'),
+    page.waitFor((locDateStr) => {
+      let newValue = document.querySelector('#ctl00_MainContent_lblTarihi').textContent
+      let matchValue = 'Vefat İlan Tarihi : ' + locDateStr
+      // console.log('New value: \t', newValue);
+      // console.log('Match value : \t', matchValue);
+      return newValue === matchValue;
+    }, {}, dateStr) // wait until DOM changes to new date. Pass dateStr to browser context as locDateStr.
+  ]);
+}
+
+/**
+ * Function to count number of death announcements in the current page.
+ * 
+ * @param {Object} page - Puppeteer page object 
+ * @returns {Number} Number of deaths
+ */
+async function countElements(page) {
+  return page.$eval('#ctl00_MainContent_UpdatePanel2 > div:nth-child(1)', (elem) => {
+    return elem.children.length - 1 // -1 for 'Vefat Ilan Tarihi....' element.
+  });
+}
+
+async function scrollToTopAndScreenshot(page, dateStr, cityName) {
+  console.log('Scrolling to top');
+  await page.evaluate(() => window.scrollTo(0, 0));
+  console.log('Taking Screenshot');
+  await page.screenshot({ path: `./screenshots/${cityName}/${dateStr}.png`, fullPage: true });
+  console.log('Took Screenshot');
+}
+
+async function savePagePDF(page, dateStr, cityName) {
+  console.log('Saving PDF');
+  return page.pdf({ path: `./pdfs/${cityName}/${dateStr}.pdf` });
+}
