e42e97e0c7201068bc662e68a2694cac3c080220
kuzdogan <kaanuzdogan@hotmail.com>
2021-03-05 12:17:44 +0100

    05.03

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 const csv = require('fast-csv');
 
 const URL = 'https://cbs.kayseri.bel.tr/kayseri-mezarlik-bilgi-sistemi';
-const DAYS_AGO = 9; // Number of days to go back.
+const DAYS_AGO = 18; // Number of days to go back.
 const CITY_NAME = 'Kayseri';
 const FILE_NAME = 'kayseri-data.csv';
 
